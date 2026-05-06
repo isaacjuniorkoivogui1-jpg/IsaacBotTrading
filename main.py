f725dc1971fd6f2baa19fc026e2b9d34993da4cf
isaacjuniorkoivogui1-jpg <isaacjuniorkoivogui1@gmail.com>
2026-05-06 14:42:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("IsaacBotTrading démarré 🚀")
+def analyse_marche(prix_actuel, support, resistance):
+    
+    if prix_actuel <= support:
+        decision = "BUY 📈"
+        confiance = "82%"
+        
+    elif prix_actuel >= resistance:
+        decision = "SELL 📉"
+        confiance = "84%"
+        
+    else:
+        decision = "ATTENDRE ⏳"
+        confiance = "50%"
+    
+    print("=== IsaacBotTrading ===")
+    print("Prix actuel :", prix_actuel)
+    print("Support :", support)
+    print("Résistance :", resistance)
+    print("Décision :", decision)
+    print("Confiance :", confiance)
+
+
+# Exemple test
+analyse_marche(
+    prix_actuel=105,
+    support=100,
+    resistance=110
+  )
